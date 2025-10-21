83718518e2d8a87326202e257485d5007f39299a
Spyderend <152734696+SpyderendD@users.noreply.github.com>
2025-10-21 18:19:43 +0300

    fix chrome

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -19,7 +19,12 @@ interface StatCardProps {
 export function StatCard({ icon, title, description, children }: StatCardProps) {
   return (
     <motion.div variants={itemVariants}>
-      <Card className="flex flex-col p-6 h-full min-h-[260px] bg-card/50">
+      {/*
+        MODIFICARE:
+        - Am scos `min-h-[260px]` pentru a permite cardului să aibă o înălțime naturală.
+        - Am păstrat `h-full` pentru a se întinde pe toată înălțimea rândului din grid.
+      */}
+      <Card className="flex flex-col p-6 h-full bg-card/50">
         
         {/* Partea de sus: Titlu și descriere */}
         <CardHeader className="p-0">
@@ -30,8 +35,12 @@ export function StatCard({ icon, title, description, children }: StatCardProps) 
           <CardDescription className="pt-1">{description}</CardDescription>
         </CardHeader>
         
-        {/* Partea de jos: Conținutul (inelul sau badge-ul) aliniat la bază */}
-        <CardContent className="p-0 flex flex-col flex-grow justify-end items-center mt-4">
+        {/* 
+          Partea de jos: Conținutul (inelul sau badge-ul).
+          MODIFICARE: `justify-end` devine `justify-center` pe mobil/tabletă și `justify-end` pe desktop.
+          Acest lucru asigură o aliniere mai bună pe ecrane mici.
+        */}
+        <CardContent className="p-0 flex flex-col flex-grow justify-center lg:justify-end items-center mt-4">
           {children}
         </CardContent>
 
